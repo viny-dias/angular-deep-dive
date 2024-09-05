054e83bb0a3f224c71aeae20b95bfa3893fac361
VINICIUS DIAS MOTA DE OLIVEIRA <52006820885@cmdo.policiamilitar.sp.gov.br>
2024-09-05 08:50:39 -0300

    breaking the app component into different components

---
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, input, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-item.component.html',
+  styleUrl: './dashboard-item.component.css'
+})
+export class DashboardItemComponent {
+  // Decorator
+  // @Input({ required: true }) image!: { src: string, alt: string }
+  // @Input({ required: true }) title!: string
+
+  //nnput function
+  image = input.required<{ src: string; alt: string }>()
+  title = input.required<string>()
+}
